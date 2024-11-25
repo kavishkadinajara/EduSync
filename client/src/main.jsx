59bad7360f1c63ad24904ec97fdac2ee303c8feb
lafesto <kavishkadinajara@gmail.com>
2024-11-25 10:22:40 +0530

    first commit

---
+++ client/src/main.jsx
@@ -0,0 +1,22 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import Root from './routes/Root';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div className="h-full min-h-screen rounded-md flex flex-col items-center justify-center relative w-screen">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
---
+++ client/src/routes/Root.jsx
@@ -0,0 +1,8 @@
+export default function Root() {
+    return (
+      <div>
+        <h1 className="text-white">Welcome to the Dashboard</h1>
+      </div>
+    );
+  }
+  
